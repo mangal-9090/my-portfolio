350f5e5fb3fbc926fe842be44c2ecfe5ae9f302e
mangal-9090 <karanmangal195@gmail.com>
2024-09-12 16:51:20 +0530

    some changes are donein Portfolio section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,39 +1,37 @@
-import React from 'react'
-import HeroImage from "../assets/heroImage.png"
-import { FiArrowRight } from "react-icons/fi";
-import { Link } from "react-scroll"
+import React from 'react';
+import HeroImage from '../assets/heroImage.png';
+import { FiArrowRight } from 'react-icons/fi';
+import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
-    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
-        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-            <div className='flex flex-col justify-center h-full'>
-                <h2 className='text-4xl sm:text-7xl font-bold text-white'>
-                    I'am a Full Stack Developer
-                </h2>
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            I'am a Full Stack Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
 
-                <p className='text-gray-500 py-4 max-w-md'>
-                    Fresher! Worked on various technologies like Html, Css, JavaScript, React, Php, Java etc.
-                    Recently participated in "Techspectra'(24 hrs code) IIT-Delhi offline hackathon.
-                    Looking forward for opportunities!!
-                    Connect with me on: linkedin 
-                </p>
-
-                <div>
-                    <Link to='portfolio' smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
-                        Portfolio
-                        <span className='group-hover:rotate-90 duration-300'><FiArrowRight size={25} className='ml-1' /></span>
-                    </Link>
-                </div>
-            </div>
-
-            <div>
-                <img src={HeroImage} alt="my profile" className='rounded-2xl mx-auto w-2/3 md:w-full' />
-            </div>
-
+            Fresher! Worked on various technologies like Html, Css, JavaScript, React, Php, Java etc.
+            Recently participated in "Techspectra'(24 hrs code) IIT-Delhi offline hackathon.
+            Looking forward for opportunities!!
+            Connect with me on: linkedin
+          </p>
+          <div>
+            <Link to="portfolio" smooth duration={500} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300"><FiArrowRight size={25} className="ml-1" /></span>
+            </Link>
+          </div>
         </div>
-    </div>
-  )
-}
 
-export default Home
+        <div className="mt-6 md:mt-0">
+          <img src={HeroImage} alt="my profile" className="rounded-2xl mx-auto w-full" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,52 +1,60 @@
-import React from 'react'
-import S1 from "../assets/portfolio/S1.png"
-import S2 from "../assets/portfolio/S2.png"
+import React from 'react';
+import S1 from "../assets/portfolio/S1.png";
+import S2 from "../assets/portfolio/S2.png";
 
 const Portfolio = () => {
-
-    const portfolios =[
-        {
-            id:1,
-            src: S1,
-            demoUrl:'http://localhost/wev1/',
-            repoUrl:'https://github.com/mangal-9090/my-college-erp-project'
-        },
-
-        {
-            id:2,
-            src: S2,
-            demoUrl:'http://localhost/Hotel-Management-System-main/index.php',
-            repoUrl:'https://github.com/mangal-9090/Hotel-Management-System-main'
-        },
-    ]
+  const portfolios = [
+    {
+      id: 1,
+      src: S1,
+      demoUrl: 'http://localhost/wev1/',
+      repoUrl: 'https://github.com/mangal-9090/my-college-erp-project',
+    },
+    {
+      id: 2,
+      src: S2,
+      demoUrl: 'http://localhost/Hotel-Management-System-main/index.php',
+      repoUrl: 'https://github.com/mangal-9090/Hotel-Management-System-main',
+    },
+  ];
 
   return (
-    <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
-        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
-                <p className='py-6'>Check out some of my work here.</p>
-            </div>
-
-
-            <div className='grid sm:grid-cols-2 md: grid-cols-3 gap-8 px-12 sm:px-0'>
-
-            {portfolios.map(({id, src, demoUrl, repoUrl}) => (
-                <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
-                <img src={src} alt="" className='rounded-md duration-200 hover:scale-105' />
-                <div className='flex items-center justify-center'>
-                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105' onClick={() => window.open(demoUrl, '_blank')}>Demo</button>
-                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105' onClick={() => window.open(repoUrl, '_blank')}>Code</button>
-                </div>
-            </div>
-                
-
-            ))}
-                
-            </div>
+    <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-full text-white">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
+          <p className="py-6">Check out some of my work here</p>
         </div>
-    </div>
-  )
-}
 
-export default Portfolio
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src, demoUrl, repoUrl }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg overflow-hidden">
+              {/* Responsive image loading */}
+              <img
+                src={src}
+                alt=""
+                className="w-full h-auto object-cover rounded-t-lg duration-200 hover:scale-105"
+              />
+              <div className="flex items-center justify-center py-4 px-6">
+                <button
+                  className="w-full sm:w-1/2 px-6 py-3 m-2 duration-200 hover:scale-105 text-center"
+                  onClick={() => window.open(demoUrl, '_blank')}
+                >
+                  Demo
+                </button>
+                <button
+                  className="w-full sm:w-1/2 px-6 py-3 m-2 duration-200 hover:scale-105 text-center"
+                  onClick={() => window.open(repoUrl, '_blank')}
+                >
+                  Code
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
