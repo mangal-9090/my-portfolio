ffd416319fa61356eb8609eb8b00c8300a2f7353
mangal-9090 <karanmangal195@gmail.com>
2024-09-13 15:22:45 +0530

    some changes are done in portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -19,31 +19,30 @@ const Portfolio = () => {
   ];
 
   return (
-    <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-full text-white">
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center">
+    <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
           <p className="py-6">Check out some of my work here</p>
         </div>
 
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 px-12 sm:px-0">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
           {portfolios.map(({ id, src, demoUrl, repoUrl }) => (
-            <div key={id} className="shadow-md shadow-gray-600 rounded-lg overflow-hidden">
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               {/* Responsive image loading */}
               <img
                 src={src}
                 alt=""
-                className="w-full h-auto object-cover rounded-t-lg duration-200 hover:scale-105"
-              />
-              <div className="flex items-center justify-center py-4 px-6">
+                className="rounded-md duration-200 hover:scale-105"/>
+              <div className="flex items-center justify-center">
                 <button
-                  className="w-full sm:w-1/2 px-6 py-3 m-2 duration-200 hover:scale-105 text-center"
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
                   onClick={() => window.open(demoUrl, '_blank')}
                 >
                   Demo
                 </button>
                 <button
-                  className="w-full sm:w-1/2 px-6 py-3 m-2 duration-200 hover:scale-105 text-center"
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
                   onClick={() => window.open(repoUrl, '_blank')}
                 >
                   Code
